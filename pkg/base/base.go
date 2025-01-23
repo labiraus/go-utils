e1e85f65225c123bf77fa009254d3e27e35c4f83
labiraus <oliver.hathaway@bjss.com>
2025-01-23 13:18:25 +0000

    Added trace_id to base

--- pkg/base/base.go
+++ pkg/base/base.go
@@ -5,30 +5,50 @@ import (
 	"log/slog"
 	"os"
 	"os/signal"
+
+	"github.com/google/uuid"
 )
+
+const TraceIDString = "trace_id"
 
 var (
 	Ready       = make(chan struct{})
 	ServiceName string
 )
 
+type ContextHandler struct {
+	slog.Handler
+}
+
+func (h *ContextHandler) Handle(ctx context.Context, r slog.Record) error {
+	if traceID, ok := ctx.Value(TraceIDString).(string); ok {
+		r.AddAttrs(slog.String(TraceIDString, traceID))
+	}
+	return h.Handler.Handle(ctx, r)
+}
+
 func Init(serviceName string) context.Context {
 	ServiceName = serviceName
-	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true})).WithGroup(serviceName))
-	slog.Info("starting")
-	ctx, ctxDone := context.WithCancel(context.Background())
 
+	baseHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true})
+	handler := &ContextHandler{Handler: baseHandler}
+	logger := slog.New(handler).WithGroup(serviceName)
+	slog.SetDefault(logger)
+
+	ctx, ctxDone := context.WithCancel(context.Background())
+	ctx = context.WithValue(ctx, TraceIDString, uuid.New())
+	slog.InfoContext(ctx, "starting")
 	go func() {
 		defer ctxDone()
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
 		s := <-c
-		slog.Info("got signal: [" + s.String() + "] now closing")
+		slog.InfoContext(ctx, "got signal: ["+s.String()+"] now closing")
 	}()
 
 	go func() {
 		<-Ready
-		slog.Info("ready")
+		slog.InfoContext(ctx, "ready")
 	}()
 
 	return ctx
